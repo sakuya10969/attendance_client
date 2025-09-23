89482db7b123501f39b6a53cd8cf5fde31be8e26
sakuya10969 <sakuya.f20010618@gmail.com>
2025-09-23 19:54:31 +0900

    feat: auth api

---
+++ api/authApi.ts
@@ -0,0 +1,11 @@
+import apiClient from '@/api/apiClient'
+
+export const signUp = async (name: string, email: string, password: string) => {
+    const response = await apiClient.post('/auth/sign-up', { name, email, password })
+    return response.data
+}
+
+export const signIn = async (email: string, password: string) => {
+    const response = await apiClient.post('/auth/sign-in', { email, password })
+    return response.data
+}
